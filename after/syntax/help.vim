derekwyatt/vim-scala

commit 844ce8699eeef706afd75f4d4e332d02a1e746c2

Wrap help file conceal usage in feature check.
This avoids an error message for vim versions < 7.3 or otherwise
compiled without the conceal feature.

after/syntax/help.vim
@@ -1,4 +1,6 @@
 let b:current_syntax = ''
 unlet b:current_syntax
 syntax include @ScalaCode syntax/scala.vim
-syntax region rgnScala matchgroup=Ignore concealends start='!sc!' end='!/sc!' contains=@ScalaCode
+if has('conceal')
+  syntax region rgnScala matchgroup=Ignore concealends start='!sc!' end='!/sc!' contains=@ScalaCode
+endif

commit b738d97b421fcb9be59501343b620c6381efe871

Fix syntax highlighting in help files.
Commit 844ce86 accidentally disabled Scala syntax highlighting
in help files if you are using a version of Vim that was
compiled without the "conceal" feature.

This patch preserves the part of 844ce86 that avoids an error
message, but also allows syntax highlighting to work.

after/syntax/help.vim
@@ -3,4 +3,6 @@ unlet b:current_syntax
 syntax include @ScalaCode syntax/scala.vim
 if has('conceal')
   syntax region rgnScala matchgroup=Ignore concealends start='!sc!' end='!/sc!' contains=@ScalaCode
+else
+  syntax region rgnScala matchgroup=Ignore start='!sc!' end='!/sc!' contains=@ScalaCode
 endif