d461e6e2badb9666e270cbce2c73c97f713769be
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2013-04-27 01:01:35 +0200

    Add specs

---
+++ spec/import_sorting_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe ":SortScalaImports" do
+
+  describe "Sorting across groups" do
+    ["vanilla"].each do |name|
+      it "should sort vanilla file" do
+        actual = sort_fixture_across_groups name
+        expected = expected(name)
+        actual.should eq(expected)
+      end
+    end
+  end
+
+end
